03534c31094e9ed68d13a2f764f352fc19ba6b4c
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-04-09 09:46:07 +0530

    initial commit

---
+++ src/redux/apiCalls.js
@@ -0,0 +1,17 @@
+import { updateStart, updateSuccess, updateError } from "./userSlice";
+import axios from "axios";
+
+
+export const updateUser = async (user, dispatch) => {
+    dispatch(updateStart());
+
+    try {
+        const res = await axios.post("http://localhost:5000/update", {
+            user
+        })
+        dispatch(updateSuccess(res.data));
+
+    } catch (error) {
+        dispatch(updateError());
+    }
+}
